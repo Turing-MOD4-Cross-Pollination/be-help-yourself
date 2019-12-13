07726a8ad6689594d4fa49bb9ec71ae13007a97b
Corina Allen <StarPerfect@gmail.com>
2019-12-13 14:35:14 -0700

    Update Knex with databases

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications_dev',
+    connection: 'postgres://localhost/cross_poll_dev',
     migrations: {
       directory: './db/migrations'
     },
@@ -15,7 +15,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: 'postgres://localhost/publications_test',
+    connection: 'postgres://localhost/cross_poll_test',
     migrations: {
       directory: './db/migrations'
     },
