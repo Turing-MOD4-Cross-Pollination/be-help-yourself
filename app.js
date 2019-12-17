6e970dc05f3aa082c6db09fbb890b3af19f502a8
Corina Allen <StarPerfect@gmail.com>
2019-12-17 16:24:40 -0700

    Update App file with GraphQL endpoint
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const graphqlHTTP = require('express-graphql');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-
-var indexRouter = require('./routes/index');
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('./knexfile')[environment];
+var db = require('./db');
+// var indexRouter = require('./routes/index');
+var schema = require('./schema.js');
 
 var app = express();
+ 
+app.use(
+  '/resources',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
+ 
+app.listen(4000);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+
+// app.use('/')
 
 
 module.exports = app;
