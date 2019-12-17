b84643ea70c628385318119cc53a9f6b5fd2b87c
Corina Allen <StarPerfect@gmail.com>
2019-12-16 17:45:10 -0700

    Update data seed file with one resource

--- db/seeds/dev/xlsData.js
+++ db/seeds/dev/xlsData.js
@@ -1,27 +1,17 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('resources').del()
-    .then(function () {
-      // Inserts seed entries
-      spreadsheet = require('xlsx-to-json');
-      spreadsheet({
-        input: "./Resources.xlsx",
-        sheet: "Clothing",
-        output: "./asd.json"
-      }, function (err, result) {
-        if (err) {
-          console.log(err);
-        } else {
-          result.forEach(element => {
-            knex('resources').insert({
-              name: element['Name'],
-              website: element['Website']
-            })
-            .then(() => console.log("Seeding complete!"))
-            .catch(error => console.log(`Error seeding data: ${error}`))
-          })
-        }
-      })
-    });
-};
+	return knex('resources').del()
+	  .then(() => {
+		return Promise.all([
+  		  knex('resources').insert({
+			    name: 'Clayton Family Futures',
+          website: 'http://www.claytonearlylearning.org/',
+          location: '3801 Martin Luther King Blvd.',
+          contact: '303-355-4411',
+          notes: 'Prenatal intervention & child development services'
+		  }, 'id')
+		  .then(() => console.log('Seeding complete!'))
+		  .catch(error => console.log(`Error seeding data: ${error}`))
+		])
+	  })
+	  .catch(error => console.log(`Error seeding data: ${error}`));
+  };
