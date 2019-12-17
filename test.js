335a618c6490839ad0322bdc8a5c653ed488435b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-16 22:31:44 -0700

    Add test file for importing resource and category information from xlsx file
    Co-Authored-By: Corina Allen <StarPerfect@gmail.com>

---
+++ test.js
@@ -0,0 +1,17 @@
+spreadsheet = require('xlsx-to-json');
+spreadsheet({
+  input: "./Resources.xlsx",
+  sheet: "Clothing",
+  output: "./asd.json"
+}, function (err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+    result.forEach(element => {
+      knex('resources').insert({
+        name: element['Name'],
+        website: element['Website']
+      })
+      .then(() => console.log("Seeding complete!"))
+      .catch(error => console.log(`Error seeding data: ${error}`))
+    })
