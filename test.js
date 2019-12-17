2db66146de1e935268d9a837d5d83766914afd7f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-17 12:26:47 -0700

    Add test file for importing data from xlsx file
    Co-Authored-By: Corina Allen <StarPerfect@gmail.com>

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 spreadsheet = require('xlsx-to-json');
 spreadsheet({
   input: "./Resources.xlsx",
@@ -8,10 +12,13 @@ spreadsheet({
     console.log(err);
   } else {
     result.forEach(element => {
-      knex('resources').insert({
+      database('resources').insert({
         name: element['Name'],
         website: element['Website']
       })
       .then(() => console.log("Seeding complete!"))
       .catch(error => console.log(`Error seeding data: ${error}`))
     })
+  }
+  }
+)
