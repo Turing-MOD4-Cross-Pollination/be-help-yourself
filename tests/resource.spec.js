bd03fec88ecb1e4087fef8d6c367d0b1d4347b64
Corina Allen <StarPerfect@gmail.com>
2019-12-17 13:38:05 -0700

    Change title of production homepage

---
+++ tests/resource.spec.js
@@ -0,0 +1,11 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('test databases', () => {
+    
+})
