4af28f828bc1d5ea993e650a49dd10f19f9e6a90
Corina Allen <StarPerfect@gmail.com>
2019-12-17 16:27:10 -0700

    Create GraphQL Schema with All Resources query
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

---
+++ schema.js
@@ -0,0 +1,50 @@
+const graphql = require('graphql')
+var db = require('./db');
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLNonNull,
+    GraphQLBoolean
+} = graphql;
+
+const CategoryType = new GraphQLObjectType({
+    name: 'Category',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString}
+    })
+})
+
+const ResourceType = new GraphQLObjectType({
+    name: 'Resource',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        website: {type: GraphQLString},
+        location: {type: GraphQLString},
+        contact: {type: GraphQLString},
+        notes: {type: GraphQLString},
+        favorited: {type: GraphQLBoolean}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        resources: {
+            type: new GraphQLList(ResourceType),
+            resolve(parents, args){
+                return db('resources').select()
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
