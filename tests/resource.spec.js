75b024abfd73339f67ae2472562cd446e2f96bad
Corina Allen <StarPerfect@gmail.com>
2019-12-17 13:50:49 -0700

    Create Resource spec

--- tests/resource.spec.js
+++ tests/resource.spec.js
@@ -7,5 +7,15 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 describe('test databases', () => {
-    
-})
+    beforeEach(async () => {
+        await database.seed.run();
+    });
+
+    describe('Resource Database', () => {
+        test('databases', async () => {
+            const firstResource = await database('resources').first();
+
+            expect(firstResource.name).toBe('Wee Cycle')
+        });
+    });
+});
