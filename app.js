f98ae326fbff60dd5d773110965b242706f94ef9
Corina Allen <StarPerfect@gmail.com>
2019-12-18 10:18:40 -0700

    Update main file - remove commented out code

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var path = require('path');
-const graphqlHTTP = require('express-graphql');
+import express from 'express';
+import path from 'path';
+import graphqlHTTP from 'express-graphql';
 
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-var db = require('./db');
-// var indexRouter = require('./routes/index');
-var schema = require('./schema.js');
+import db from './db';
+import schema from './schema.js';
 
 var app = express();
  
@@ -20,15 +17,4 @@ app.use(
  
 app.listen(4000);
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-
-// app.use('/')
-
-
-module.exports = app;
+export default app;
