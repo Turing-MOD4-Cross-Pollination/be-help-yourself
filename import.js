956f9a3aa5eeca66d019888d159e94e7d969310f
Corina Allen <StarPerfect@gmail.com>
2019-12-19 10:59:38 -0700

    Add spacing for readability

--- import.js
+++ import.js
@@ -2,29 +2,30 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 spreadsheet = require('xlsx-to-json');
+
 spreadsheet({
   input: "./Resources.xlsx",
   sheet: "Sheet1",
   output: "./resouces_import.json"
-}, function (err, result) {
-  if (err) {
-    console.log(err);
-  } else {
-    result.forEach(element => {
-      database('resources').del().then(function () {
-      database('resources').insert({
-        name: element['name'],
-        website: element['website'],
-        location: element['location'],
-        contact: element['contact'],
-        notes: element['notes'],
-        category: element['category'],
-        subcategory: element['subcategory']
-      })
-      .then(() => console.log("Seeding complete!"))
-      .catch(error => console.log(`Error seeding data: ${error}`))
-    })
-  });
-  }
+  }, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      result.forEach(element => {
+        database('resources').del().then(function () {
+        database('resources').insert({
+            name: element['name'],
+            website: element['website'],
+            location: element['location'],
+            contact: element['contact'],
+            notes: element['notes'],
+            category: element['category'],
+            subcategory: element['subcategory']
+          })
+        .then(() => console.log("Seeding complete!"))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+        })
+      });
+    }
   }
 )
