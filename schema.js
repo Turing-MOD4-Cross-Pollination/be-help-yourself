680cc157e5e50887f5ee4dcb985609b24c9146d4
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-19 15:26:08 -0700

    Update schema.js with graphql queries for resources based on all resources and their categories, cities, zip_code, subcategories, and favorited status
    Co-Authored-By: Corina Allen <StarPerfect@gmail.com>

--- schema.js
+++ schema.js
@@ -12,14 +12,6 @@ const {
     GraphQLBoolean
 } = graphql;
 
-const CategoryType = new GraphQLObjectType({
-    name: 'Category',
-    fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString}
-    })
-})
-
 const ResourceType = new GraphQLObjectType({
     name: 'Resource',
     fields: () => ({
@@ -29,6 +21,8 @@ const ResourceType = new GraphQLObjectType({
         location: {type: GraphQLString},
         contact: {type: GraphQLString},
         notes: {type: GraphQLString},
+        category: {type: GraphQLString},
+        subcategory: {type: GraphQLString},
         favorited: {type: GraphQLBoolean}
     })
 })
@@ -41,6 +35,48 @@ const RootQuery = new GraphQLObjectType({
             resolve(parents, args){
                 return db('resources').select()
             }
+        },
+        resource: {
+          type: ResourceType,
+          args: {category: {type: GraphQLString}},
+          resolve(parent, args){
+            return db('resources').where('category', args.category).first()
+          }
+        },
+        resources_by_category: {
+          type: new GraphQLList(ResourceType),
+          args: {category: {type: GraphQLString}},
+          resolve(parent, args){
+            return db('resources').where('category', args.category).select()
+          }
+        },
+        resources_by_city: {
+          type: new GraphQLList(ResourceType),
+          args: {city: {type: GraphQLString}},
+          resolve(parent, args){
+            return db('resources').where('city', args.city).select()
+          }
+        },
+        resources_by_zip_code: {
+          type: new GraphQLList(ResourceType),
+          args: {zip_code: {type: GraphQLString}},
+          resolve(parent, args){
+            return db('resources').where('zip_code', args.zip_code).select()
+          }
+        },
+        resources_by_subcategory: {
+          type: new GraphQLList(ResourceType),
+          args: {subcategory: {type: GraphQLString}},
+          resolve(parent, args){
+            return db('resources').where('subcategory', args.subcategory).select()
+          }
+        },
+        resources_by_favorited: {
+          type: new GraphQLList(ResourceType),
+          args: {favorited: {type: GraphQLBoolean}},
+          resolve(parent, args){
+            return db('resources').where('favorited', args.favorited).select()
+          }
         }
     }
 })
