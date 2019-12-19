867ad4e7aad99fa6932d2b292559dc9231f3c6a4
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-19 10:50:47 -0700

    Change migration so that there is only one resources table, add category and subcategory attributes, and add 1000 character length to website and notes attributes
    Co-Authored-By: Corina Allen <StarPerfect@gmail.com>

--- db/migrations/20191216133436_initial.js
+++ db/migrations/20191216133436_initial.js
@@ -4,35 +4,21 @@ exports.up = function(knex) {
     knex.schema.createTable('resources', function(table) {
         table.increments('id').primary();
         table.string('name');
-        table.string('website');
+        table.string('website', 1000);
         table.string('location');
         table.string('contact');
-        table.string('notes');
+        table.string('notes', 1000);
+        table.string('category');
+        table.string('subcategory');
         table.boolean('favorited');
 
         table.timestamps(true, true);
-    }),
-
-    knex.schema.createTable('categories', function(table) {
-      table.increments('id').primary();
-      table.string('name');
-
-      table.timestamps(true, true);
-    }),
-
-    knex.schema.createTable('category_resources', function(table){
-      table.increments().primary();
-      table.integer('categories_id').references('id').inTable('categories');
-      table.integer('resources_id').references('id').inTable('resources');
-      table.timestamps(true, true);
     })
   ])
 };
 
 exports.down = function(knex) {
   return Promise.all([
-    knex.schema.dropTable('category_resources'),
-    knex.schema.dropTable('resources'),
-    knex.schema.dropTable('categories')
+    knex.schema.dropTable('resources')
   ]);
 };
