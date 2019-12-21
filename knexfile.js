68b9d025d99f6594e4b75212a38330b090fd05ec
Corina Allen <StarPerfect@gmail.com>
2019-12-20 18:44:39 -0700

    Remove seeds from Knexfile environments since we longer have seeds - this also means our test wont pass so need to rethink before this merge can happen

--- knexfile.js
+++ knexfile.js
@@ -8,9 +8,6 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
-    seeds: {
-      directory: './db/seeds/dev'
-    },
     useNullAsDefault: true
   },
   test: {
@@ -18,9 +15,6 @@ module.exports = {
     connection: 'postgres://localhost/community_connect_test',
     migrations: {
       directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
     },
     useNullAsDefault: true
   },
@@ -37,9 +31,6 @@ module.exports = {
     connection: 'postgres://rliwwgvberzagx:440b196e46606c228a179cc597fca632371526ed795cefffee8c6707082a259c@ec2-184-73-169-163.compute-1.amazonaws.com:5432/dftv5q2jck251l',
     migrations: {
       directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
     },
     useNullAsDefault: true
   }
