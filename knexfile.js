b93f7d0ace3f5adabd6d7c59690ffe165aedd627
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-08 16:33:07 -0700

    Put database url in production environment

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://tsvbmuqbarhdoz:1aabdc857196af23bed2af1c688ac8579c24093a5dd0f2ad92c041b92d2a59ec@ec2-174-129-33-88.compute-1.amazonaws.com:5432/d3ulh8q9euvlqg',
+    connection: process.env.DATABASE_URL || 'postgres://vjspyablvwgxek:f0cb940e34d7a21e6a505721db9bf56f86bd930ae645b3f92b345ff3c4653394@ec2-174-129-33-88.compute-1.amazonaws.com:5432/d7cv8o9kpo3cfn',
     migrations: {
       directory: './db/migrations'
     },
