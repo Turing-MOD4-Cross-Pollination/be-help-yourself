0e5e62631ce57bc0714b075d5c0289699847f42a
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-08 09:29:41 -0700

    Create recovery meetings table with new migration
    Co-Authored-By: Corina Allen <StarPerfect@gmail.com>

---
+++ db/migrations/20200108092739_recovery_meetings.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('recovery', function(table) {
+        table.increments('id').primary();
+        table.string('title', 1000);
+        table.string('subtitle', 1000);
+        table.string('adddress', 1000);
+
+        table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('recovery')
+  ]);
+};
