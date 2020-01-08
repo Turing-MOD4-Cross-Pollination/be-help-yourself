e95d6ef2f82fc9e0aefb4b26f3cb4f10e50d27a6
Joshua Sherwood <jkjgsherwood@gmail.com>
2020-01-08 16:40:08 -0700

    Only have database env variable for production connection

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://vjspyablvwgxek:f0cb940e34d7a21e6a505721db9bf56f86bd930ae645b3f92b345ff3c4653394@ec2-174-129-33-88.compute-1.amazonaws.com:5432/d7cv8o9kpo3cfn',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
